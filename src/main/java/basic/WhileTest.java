68700e78b231d630c846f2ea155e6d5423da974d
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-02-09 23:24:25 +0900

    Test

--- src/main/java/basic/WhileTest.java
+++ src/main/java/basic/WhileTest.java
@@ -1,11 +1,11 @@
-package basic;
-
-public class WhileTest {
-    public static void main(String[] args){
-        int x = 1, y = 6;
-        while(y--){
-            x++;
-        }
-        System.out.println("x = "+x+"y = " + y);
-    }
-}
+//package basic;
+//
+//public class WhileTest {
+//    public static void main(String[] args){
+//        int x = 1, y = 6;
+//        //while(y--){
+//            x++;
+//        }
+//        //System.out.println("x = "+x+"y = " + y);
+//    }
+//}
